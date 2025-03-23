72ee3d3bb1d6ac9d2551dddb06dce33c81bab2a8
skalaga-arch <159321558+skalaga-arch@users.noreply.github.com>
2025-03-22 22:03:27 -0400

    documentation_generated

--- caluclator/docs/conf.py
+++ caluclator/docs/conf.py
@@ -2,6 +2,9 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -14,7 +17,8 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +28,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
---
+++ caluclator/docs/source/calculator.rst
@@ -0,0 +1,29 @@
+calculator package
+==================
+
+Submodules
+----------
+
+calculator.core module
+----------------------
+
+.. automodule:: calculator.core
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+calculator.utils module
+-----------------------
+
+.. automodule:: calculator.utils
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: calculator
+   :members:
+   :show-inheritance:
+   :undoc-members:
